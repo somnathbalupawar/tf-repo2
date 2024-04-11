9c2ebb51c230bfaa19fa88ad2a25b2bc2edba0c3
root <root@ip-172-31-89-146.ec2.internal>
2024-04-11 03:19:14 +0000

    manifest

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+terraform {
+	backend "s3" {
+		bucket = "my-tf-test-bucket-cis"
+		key = "terraform.tfstate"
+		region = "us-east-1"
+	}
+}
+
+resource "aws_instance" "inst1" {
+	ami = "ami-0cd59ecaf368e5ccf"
+	instance_type = "t2.micro"
+}
